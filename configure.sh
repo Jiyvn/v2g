f6de176da9d2b600d17801608a446a58e130fb88
jiyvn <73217816+Jiyvn@users.noreply.github.com>
2022-10-30 18:08:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ mkdir /tmp/v2ray
 curl -L -H "Cache-Control: no-cache" -o /tmp/v2ray/v2ray.zip https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/v2ray-linux-64.zip
 unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
 install -m 755 /tmp/v2ray/v2ray /usr/local/bin/v2ray
-#install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
+install -m 755 /tmp/v2ray/v2ctl /usr/local/bin/v2ctl
 
 # Remove temporary directory
 rm -rf /tmp/v2ray
